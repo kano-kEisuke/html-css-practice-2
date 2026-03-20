f1bfeeea3ebaabf397786a5f19dcefb62e1d8a77
keisukE <kano5guitar@gmail.com>
2026-03-21 02:53:17 +0900

    クリックしたやつの色変える処理

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,22 @@
 // テスト用でボタン押したらアラート出すやつ
 const buttons = document.querySelectorAll('.button');
-buttons.forEach(function(btn) {
-  btn.addEventListener('click', function() {
-    alert('クリックされました！');
-  });
+buttons.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        alert('クリックされました！');
+    });
 });
 
+
+//grid-cols-4 .grid-itemの要素を全て取得してitemsに格納
+const items = document.querySelectorAll('.grid-cols-4 .grid-item');
+
+
+items.forEach(function (item) { //変数itemsの要素を全てループして、itemに格納する
+    item.addEventListener('click', function () { //itemに格納された要素がクリックされるのを待ち構える
+        //以下がクリックされたときの処理
+        items.forEach(function (allitem) { //変数itemsの要素を全てループして、allitemに格納する
+            allitem.classList.remove('active'); //allitemに格納された要素からactiveクラスを削除する
+        });
+        item.classList.add('active'); //クリックされたitemにactiveクラスを追加する
+    });
+});
